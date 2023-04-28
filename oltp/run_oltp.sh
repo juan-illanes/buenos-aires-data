aaf19a221ca8d0464b66ac20329a9c77b63deda8
Juan Illanes <juan.illanes.gauto@gmail.com>
2023-04-27 22:01:56 -0300

    Adding scripts to execute the workflow

--- oltp/run_oltp.sh
+++ oltp/run_oltp.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "0a. Building the postgres image..."
+docker build -t oltp .
+echo "... done!"
+
+echo "0b. Starting the postgres container..."
+docker run --name oltp -e POSTGRES_PASSWORD=should_be_a_secret -p 5432:5432 -d oltp
+echo "... done!"
+
 echo "1a. Creating the virtual environment..."
 python3 -m venv .env
 echot "... done!"
@@ -12,11 +20,10 @@ echo "1c. Installing dependencies..."
 pip install -r requirements.txt
 echo "... done!"
 
-echo "2. Launching the ETL process defined with Luigi..."
-python -m luigi --module main Load --local-scheduler
+echo "2. Processing the CSV file and populating the DB..."
+python oltp.py
 echo "... done!"
 
 echo "3. Deactivating the virtual environment..."
 deactivate
 echo ".. done!"
-
